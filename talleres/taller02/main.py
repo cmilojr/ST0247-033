57f2b695d1661621360ee8bcf7800e343b515c5f
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2019-02-24 22:29:13 -0500

    Add files via upload

---
+++ talleres/taller02/main.py
@@ -0,0 +1,23 @@
+from bfa import *
+
+"""
+Pruebas de implementacion de BFA
+"""
+test = Bfa()
+
+conjunto = "abcd"
+test.subConjuntos(conjunto)
+
+print()
+
+test.permutaciones(conjunto)
+
+print()
+
+conjunto = "melo"
+test.subConjuntos(conjunto)
+
+print()
+
+test.permutaciones(conjunto)
+
